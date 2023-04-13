b62955c320ecb7036b2d4410acae375f2e2b21e3
spuro <36612221+spuro@users.noreply.github.com>
2023-04-13 18:38:21 +0930

    next api input typing

--- yayocorprarity/src/pages/api/rarity/[id].ts
+++ yayocorprarity/src/pages/api/rarity/[id].ts
@@ -1,10 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import rarityResults from "../../../data/rarityResults.json";
 
-export default function handler(req, res) {
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
   req.statusCode = 200;
   console.log(req.query);
   res.send({
+    //@ts-ignore
     id: rarityResults[req.query.id],
   });
 }
